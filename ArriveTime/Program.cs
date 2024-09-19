fa2c6933ccc8e9a5e9249ac35da319cad43fc1e8
Bozhidar-Yovchev <bozhidar601@gmail.com>
2024-09-19 21:38:56 +0300

    Time saver

---
+++ ArriveTime/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArriveTime
+{
+    class Program
+    {
+        static string Arrive(int hour, int minutes, int arrivehour, int arriveminutes)
+        {
+            if (hour == arrivehour)
+            {
+                if (minutes == arriveminutes)
+                {
+                    return "On time";
+                }
+            }
+            if (hour <= arrivehour)
+            {
+
+                if (arrivehour - hour == 0)
+                {
+                    if (arriveminutes - minutes > 0)
+                    {
+                        return $"Late. {arriveminutes - minutes} minutes after the start";
+                    }
+                    else { return $"Late. {arrivehour - hour} hour and {arriveminutes - minutes} minutes after the start"; }
+
+                }
+                else { return $"Late. {arrivehour - hour} hour and {arriveminutes - minutes} minutes after the start"; }
+            }
+            if (hour > arrivehour)
+            {
+
+                return $"Early. {hour - arrivehour} hour and {minutes - arriveminutes} minutes before the start";
+            }
+            return default;
+        }
+        static void Main(string[] args)
+        {
+
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int arrivehour = int.Parse(Console.ReadLine());
+            int arriveminutes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Arrive(hour, minutes, arrivehour, arriveminutes));
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSaver
+{
+    class Program
+    {
+        static double Converter(double num, string first, string second)
+        {
+
+            if (first == "mm")
+            {
+                if (second == "m")
+                {
+                    return num /= 1000;
+                }
+                else if (second == "cm")
+                {
+                    return num /= 100;
+                }
+            }
+            else if (first == "m")
+            {
+                if (second == "cm")
+                {
+                    return num *= 100;
+                }
+                else if (second == "mm")
+                {
+                    return num *= 1000;
+                }
+            }
+            else if (first == "cm")
+            {
+                if (second == "m")
+                {
+                    return num /= 100;
+                }
+                else if (second == "mm")
+                {
+                    return num *= 10;
+                }
+            }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number:");
+            double num = double.Parse(Console.ReadLine());
+            Console.WriteLine("First unit of measurement:");
+            string first = Console.ReadLine();
+            Console.WriteLine("Convert to:");
+            string second = Console.ReadLine();
+
+            Console.WriteLine($"{Converter(num, first, second):f3}");
+        }
+    }
+}
